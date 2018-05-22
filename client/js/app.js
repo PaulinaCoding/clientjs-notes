f8bf96fce329c2f9189dbde68e939348de65611c
PaulinaCoding <37420244+PaulinaCoding@users.noreply.github.com>
2018-05-22 10:49:05 -0700

    Update app.js

--- client/js/app.js
+++ client/js/app.js
@@ -8,12 +8,22 @@ function addTodo(content) {
 
 function setupEventListeners() {
   var button = document.querySelector('button[name=add]');
+  //selecting specific button by name - add is a name
   button.addEventListener('click', function(event) {
+    //test with console.log('test') but this shown for only a milisecond and ther reload this is a defualt behaviour
     event.preventDefault();
+    // we stop this default reload with this line
 
     var content = document.querySelector('input[name=content]');
     if(content.value !== '') {
       addTodo(content.value);
+      
+      //content is the input we are grabing data from
+      //querySelector works more like jqury. more specific than document.get element
+      // console.log(content.value); to check it
+      //value was one of the DOM objects/imputs
+      //you will be using this while building react apps to not to build entire library of the jqury
+      //not to use jquery 
     }
     content.value = '';
   });
@@ -21,4 +31,5 @@ function setupEventListeners() {
 
 document.addEventListener('DOMContentLoaded', function(event) {
   setupEventListeners();
+  //the way to keep the js in the head
 });
